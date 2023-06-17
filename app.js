6a0d98363226c61e56a54d2ec17b02780c783a61
tejeshwar.k <tejeshwar.k@blockchainsolutions.network>
2023-06-17 10:13:26 +0530

    build: setup MVC structure

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const dbConfig = require('./config/mongodb/config.js')
 
 const app = express();
 
+express.application.prefix = express.Router.prefix = function (path, configure) {
+    var router = express.Router();
+    this.use(path, router);
+    configure(router);
+    return router;
+};
+
+app.use('/', require('./routes'));
+
 // Start the server
 const port = process.env.APP_PORT;
 app.listen(port, () => {
