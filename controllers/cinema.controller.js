9a8faa68684ec9c475aa549f9e92a03e5b96f073
tejeshwar.k <tejeshwar.k@blockchainsolutions.network>
2023-06-17 12:05:51 +0530

    feat: add consecutive seat booking API

--- controllers/cinema.controller.js
+++ controllers/cinema.controller.js
@@ -1,10 +1,11 @@
 const Cinema = require('../models/cinema.model').Cinema;
+const helper = require('../services/helper');
 
 exports.createCinema = async (req, res) => {
     const seats = req.body.seats;
 
     try {
-      const cinema = await Cinema({ seats });
+      const cinema = await Cinema({ limit: seats });
       await cinema.save();
   
       res.json({ cinemaId: cinema._id });
@@ -26,11 +27,37 @@ exports.purchaseCinemaTicket = async (req, res) => {
       return res.status(400).json({ error: 'Seat already purchased' });
     }
 
+    if(seat > cinema.limit) {
+        return res.status(400).json({ error: 'Seat Not Available' });
+    }
+
     cinema.seats.push(Number(seat));
     await cinema.save();
 
     res.json({ seat });
   } catch (error) {
     res.status(500).json({ error: 'Failed to purchase seat' });
+  }
+};
+
+exports.purchaseConsecutiveCinemaTicket = async (req, res) => {
+    const { cinemaId } = req.params;
+
+  try {
+    const cinema = await Cinema.findById(cinemaId);
+    if (!cinema) {
+      return res.status(404).json({ error: 'Cinema not found' });
+    }
+
+    for(let i = 1; i < cinema.limit; i++){
+        if(!cinema.seats.includes(Number(i)) && !cinema.seats.includes(Number(i + 1)) ){
+            await helper.reserveSeats(cinemaId, [i, i+1]);
+            return res.status(200).json({'seats booked' : [i, i+1] })
+        }
+    }
+
+    res.status(500).json({ error: 'No Consecutive seats available' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to purchase seats' + error });
   }
 };
--- routes/cinemaRoutes.js
+++ routes/cinemaRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const cinemaController = require('../controllers/cinema.controller')
 
+// Create a cinema with N seats
 router.post('/create', cinemaController.createCinema);
+// Purchase a specific seat number in cinema
 router.post('/purchase/:cinemaId/:seat', cinemaController.purchaseCinemaTicket);
+// Purchase the first two free consecutive seats in cinema C
+router.post('/purchase-consecutive/:cinemaId/', cinemaController.purchaseConsecutiveCinemaTicket)
 
 
 module.exports = router;
